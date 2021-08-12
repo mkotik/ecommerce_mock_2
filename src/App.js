f2b3a14331a6ce1263b39e311fda5ccb3ba637ee
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-08-12 17:52:55 -0400

    test

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App(props) {
   return (
     <div>
       <Navbar />
-      {/* <Showcase /> */}
-      {/* <Products /> */}
+      <Showcase />
+      <Products />
       <Cart />
     </div>
   );
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,27 +8,29 @@ function Cart(props) {
   }, [props]);
   return (
     <section className="section-car p-5">
-      <div className="container">
-        <h1>Shopping Cart</h1>
-        <div className="row items-header text-start mb-2">
-          <div className="col-5 ">Product</div>
-          <div className="col ">Price</div>
-          <div className="col ">Quantity</div>
-          <div className="col ">Total</div>
+      {props.cart.line_items && (
+        <div className="container">
+          <h1>Shopping Cart</h1>
+          <div className="row items-header text-start mb-2">
+            <div className="col-5 ">Product</div>
+            <div className="col ">Price</div>
+            <div className="col ">Quantity</div>
+            <div className="col ">Total</div>
+          </div>
+          {props.cart.line_items.map((item, i) => {
+            return (
+              <CartItem
+                image={item.media.source}
+                name={item.name}
+                price={item.price.formatted_with_symbol}
+                quantity={item.quantity}
+                total={item.line_total.formatted_with_symbol}
+                key={i}
+              />
+            );
+          })}
         </div>
-        {props.cart.line_items.map((item, i) => {
-          return (
-            <CartItem
-              image={item.media.source}
-              name={item.name}
-              price={item.price.formatted_with_symbol}
-              quantity={item.quantity}
-              total={item.line_total.formatted_with_symbol}
-              key={i}
-            />
-          );
-        })}
-      </div>
+      )}
     </section>
   );
 }
